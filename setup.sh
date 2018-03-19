ccb5ddb6381e10c546838bb27ba056c7d219ccc1
Dave Laungani <d7laungani@hotmail.com>
2018-03-18 23:56:07 -0500

    php 7.2

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ apt-get install percona-server-server-5.7 -q -y
 
 apt-get install nginx -y
 apt-get install php -y
-apt-get install -y tmux curl wget nginx php7.0-fpm php7.0-cli php7.0-curl php7.0-gd php7.0-intl 
-apt-get install -y php7.0-mysql php-memcached php7.0-mbstring php7.0-zip php7.0-xml
+apt-get install -y tmux curl wget nginx php7.2-fpm php7.2-cli php7.2-curl php7.2-gd php7.2-intl 
+apt-get install -y php7.2-mysql php-memcached php7.2-mbstring php7.2-zip php7.2-xml
